a67f64917a20a05f4ab8c418f579cb51a4e970c4
Scholtz <ludko2@gmail.com>
2025-06-29 17:22:17 +0200

    allow swagger in prod

--- BiatecTokensApi/Program.cs
+++ BiatecTokensApi/Program.cs
@@ -11,14 +11,14 @@ namespace BiatecTokensApi
 
             // Add services to the container.
             builder.Services.AddControllers();
-            
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
-                { 
-                    Title = "Biatec Tokens API", 
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Biatec Tokens API",
                     Version = "v1",
                     Description = "API for deploying and managing ERC20 tokens on Base blockchain"
                 });
@@ -27,18 +27,14 @@ namespace BiatecTokensApi
             // Configure blockchain settings
             builder.Services.Configure<BlockchainConfig>(
                 builder.Configuration.GetSection("BlockchainConfig"));
-            
+
             // Register the token service
             builder.Services.AddScoped<ITokenService, TokenService>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
